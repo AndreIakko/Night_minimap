4c080bdf90ff23d82171f26e7e7977d1edfa1640
AndreIakko <63969450+AndreIakko@users.noreply.github.com>
2021-10-02 14:25:49 +0200

    Add files via upload

---
+++ iak_night_minimap/client.lua
@@ -0,0 +1,18 @@
+Citizen.CreateThread(function()
+    SetMapZoomDataLevel(0, 0.96, 0.9, 0.08, 0.0, 0.0) -- Level 0
+    SetMapZoomDataLevel(1, 1.6, 0.9, 0.08, 0.0, 0.0) -- Level 1
+    SetMapZoomDataLevel(2, 8.6, 0.9, 0.08, 0.0, 0.0) -- Level 2
+    SetMapZoomDataLevel(3, 12.3, 0.9, 0.08, 0.0, 0.0) -- Level 3
+    SetMapZoomDataLevel(4, 22.3, 0.9, 0.08, 0.0, 0.0) -- Level 4
+end)
+
+Citizen.CreateThread(function()
+    while true do
+		Citizen.Wait(1)
+		if IsPedOnFoot(PlayerPedId()) then 
+			SetRadarZoom(1100)
+		elseif IsPedInAnyVehicle(PlayerPedId(), true) then
+			SetRadarZoom(1000)
+		end
+    end
+end)
---
+++ iak_night_minimap/fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version "bodacious"
+game 'gta5'
+author 'Iakkoise'
+
+client_script 'client.lua'
